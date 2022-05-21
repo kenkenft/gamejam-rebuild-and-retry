911098a5d6733d03349f859688fc1b290e98bf85
Ken Tang <kenftang@gmail.com>
2022-05-21 21:10:50 +0100

    Add player jump functionality
    Player can jump when the spacebar key is pressed. There isn't a check
    for whether the player is on the ground when the jump occurs.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     
-    [SerializeField] float playerSpeed = 1.0f;
+    [SerializeField] float playerSpeed = 0.05f;
+    [SerializeField] float playerJump = 1.0f;
+    public LayerMask groundLayerMask;
+
+    private Rigidbody2D rig;
+
+    void Awake ()
+    {
+        // Get out components
+        rig = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
@@ -17,5 +27,40 @@ public class Player : MonoBehaviour
         
         float moveAmount = Input.GetAxis("Horizontal") * playerSpeed;
         transform.Translate(moveAmount, 0, 0);
+        if(Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
     }
+
+
+    void Jump()
+    {
+        // if(IsGrounded())
+            // {
+                // Add force upwards to rigidbody 
+                rig.AddForce(Vector2.up * playerJump, ForceMode2D.Impulse);
+            // }
+    }
+
+    // bool IsGrounded ()
+    // {
+    //     // A raycast will be sent from 4 positions. This way, the player can still jump, even when they're on the edge 
+        
+    //     Ray2D[] rays = new Ray2D[1]
+    //     {
+    //         new Ray2D(transform.position + (Vector3.up * 0.01f), Vector3.down),             // Sends raycast 0.2m from the front of player's feet
+            
+    //     };
+
+    //     // Check whether part of the player is on ground
+    //     for(int i = 0; i < rays.Length; i++)
+    //     {
+    //         if(Physics2D.Raycast(rays[i], 0.1f, groundLayerMask))
+    //         {
+    //             return true;                // Player is on ground
+    //         }
+    //     }
+
+    //     return false;
+    // }
 }
