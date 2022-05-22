bef8670b4e04b386ce4a6257bc70ac27f7a77d78
Ken Tang <kenftang@gmail.com>
2022-05-22 17:39:33 +0100

    Add TimedGate class Add OpenTheGate method in TimedGate class Add OpenTheGate call in Interactable class
    Created a new class, TimedGate, that will eventually allow gates
    to be temporarily open. This assumes that the Switch object is a child
    of the object that has TimedGate.
    
    In Interactable class, the method PullSwitch searches in the parent
    objects until one has the TimedGate component. Then it will call
    TimedGate method OpenTheGate. At the moment, OpenTheGate only prints a
    debug message.

--- Assets/Scripts/TimedGate.cs
+++ Assets/Scripts/TimedGate.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TimedGate : MonoBehaviour
 {
+    private BoxCollider2D gateCollider;
+    public float timeOpen = 5.0f;
+    private SpriteRenderer gateSprite;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gateCollider = GetComponent<BoxCollider2D>();
+        gateSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OpenTheGate()
+    {
+        Debug.Log("Door is Open!");
     }
 }
