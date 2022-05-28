ec6403d2bdc63834bb45a3d114d065505c203ac1
Ken Tang <kenftang@gmail.com>
2022-05-28 16:14:24 +0100

    Add method of loading scene in Enemy
    The end goal of level1_scene2 is to reduce Death's hp to 0.
    On doing so, SetSpawnDoor is called to load the next scene,
    level1_scene3.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -34,6 +34,10 @@ public class Enemy : MonoBehaviour
             // anim.SetTrigger("Die");
             GetComponent<BoxCollider2D>().enabled = false;             // Remove collider on death
             // GetComponent<Rigidbody2D>().isKinematic = true;             // Remove collider on death
+
+            DoorManager doorManager = FindObjectOfType<DoorManager>();
+            Door targetDoor = gameObject.GetComponent<Door>();
+            doorManager.SetSpawnDoor(targetDoor);   // Call method that saves information into PlayerPrefs and goes to next scene
         }
     }
 }
