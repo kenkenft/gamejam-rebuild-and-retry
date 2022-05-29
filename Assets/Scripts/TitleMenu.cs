611b96b856f824f2c66ecfccacbdd5c06e856bb5
Ken Tang <kenftang@gmail.com>
2022-05-29 21:28:19 +0100

    Add title menu scenes Add TitleMenu Class for title menu scene Fix issue in level3_scene1 for spawn
    Added title menu scene. Currently, it starts the main game. Added
    TitleMenu class for this scene.
    
    Reordered Spawn gameobject location in DeadlyStuff object in
    level3_scene1. Door class was spawning the user at the first GameObject
    child in hierarchy, which was the Container gameobject. This would
    position the player on the container, which is also where the loadscene
    trigger is.

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using DictKeyCheck;
+
+public class TitleMenu : MonoBehaviour
+{
+    //Needs an EventSystem Component, otherwise these button commands don't work
+    public void OnPlayButton ()
+    {
+        //Load level 1
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButton ()
+    {
+        //When testing in Unity, it will not actually quit the Unity application itself
+        Application.Quit();
+    }
+
+    public void OnReturnButton ()
+    {
+        //Returns player to Main menu from End screen
+        SceneManager.LoadScene(0);
+    }
+}
